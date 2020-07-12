hushmirror/SilentDragon

use correct wormhole, derp

src/websockets.cpp
@@ -112,7 +112,8 @@ void WormholeClient::connect() {
     QObject::connect(m_webSocket, &QWebSocket::connected, this, &WormholeClient::onConnected);
     QObject::connect(m_webSocket, &QWebSocket::disconnected, this, &WormholeClient::closed);
 
-    m_webSocket->open(QUrl("wss://wormhole.zecqtwallet.com:443"));
+    m_webSocket->open(QUrl("wss://wormhole.myhush.org:443"));
+    //TODO: use env var to over-ride
     //m_webSocket->open(QUrl("ws://127.0.0.1:7070"));
 }
 

OleksandrBlack/safecoin-qt-wallet

src/websockets.cpp
@@ -142,10 +142,11 @@ void WormholeClient::connect() {
         m_webSocket->open(wormhole);
         qDebug() << "Opened connection to " << wormhole;
         //TODO: use env var to over-ride
-        //m_webSocket->open(QUrl("ws://127.0.0.1:7070"));
+        m_webSocket->open(QUrl("ws://wormhole.safecoin.org:443"));
     } else {
         qDebug() << "Invalid websocket object!";
     }
+
 
 }
 