hushmirror/SilentDragon

Fix mkdir bug, thanks @radix42!

src/connection.cpp
@@ -174,7 +174,7 @@ void ConnectionLoader::createZcashConf() {
     }
 
     main->logger->write("Creating file " + confLocation);
-    QDir().mkdir(fi.dir().absolutePath());
+    QDir().mkpath(fi.dir().absolutePath());
 
     QFile file(confLocation);
     if (!file.open(QIODevice::ReadWrite | QIODevice::Truncate)) {
@@ -187,7 +187,7 @@ void ConnectionLoader::createZcashConf() {
 
     QTextStream out(&file);
 
-    out << "# Autogenerated by Silent Dragon\n";
+    out << "# Autogenerated by Hush SilentDragon https://myhush.org\n";
     out << "server=1\n";
     out << "rpcuser=hush\n";
     out << "rpcpassword=" % randomPassword() << "\n";

OleksandrBlack/safecoin-qt-wallet

src/connection.cpp
@@ -187,10 +187,11 @@ void ConnectionLoader::createZcashConf() {
         this->showError(explanation);
         return;
     }
-        
-    QTextStream out(&file); 
-    
-    out << "# Autogenerated by SafecoinWallet\n";
+
+    QTextStream out(&file);
+
+    out << "# Autogenerated by Safecoin SafeWallet https://safecoin.org\n";
+
     out << "server=1\n";
     out << "rpcuser=safecoin\n";
     out << "rpcpassword=" % randomPassword() << "\n";