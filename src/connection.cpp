hushmirror/SilentDragon

This variant of ::exists correctly handles non-existing symlinks

src/connection.cpp
@@ -347,7 +347,8 @@ bool ConnectionLoader::startEmbeddedZcashd() {
     auto hushdProgram = appPath.absoluteFilePath("komodod");
 #endif
     
-    if (!QFile(hushdProgram).exists()) {
+    //if (!QFile(hushdProgram).exists()) {
+    if (!QFile::exists(hushdProgram)) {
         qDebug() << "Can't find hushd at " << hushdProgram;
         main->logger->write("Can't find hushd at " + hushdProgram);
         return false;

OleksandrBlack/safecoin-qt-wallet

src/connection.cpp
@@ -365,9 +365,12 @@ bool ConnectionLoader::startEmbeddedZcashd() {
     auto safecoindProgram = appPath.absoluteFilePath("safecoind");
 #endif
     
-    if (!QFile(safecoindProgram).exists()) {
-        qDebug() << "Can't find safecoind at " << safecoindProgram;
-        main->logger->write("Can't find safecoind at " + safecoindProgram); 
+
+    //if (!QFile(hushdProgram).exists()) {
+    if (!QFile::exists(safecoindProgram)) {
+        qDebug() << "Can't find hushd at " << safecoindProgram;
+        main->logger->write("Can't find hushd at " + safecoindProgram);
+
         return false;
     }
 