hushmirror/SilentDragon

Use animated logo for sending and shutting down, we can use custom graphics later on

src/sendtab.cpp
@@ -711,7 +711,11 @@ void MainWindow::sendButton() {
         auto d = new QDialog(this);
         auto connD = new Ui_ConnectionDialog();
         connD->setupUi(d);
-        QPixmap logo(":/img/res/logobig.gif");
+        QMovie *movie = new QMovie(":/img/res/silentdragon-animated.gif");
+        connD->topIcon->setMovie(movie);
+        movie->start();
+
+
         //connD->topIcon->setBasePixmap(logo.scaled(256, 256, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 
         connD->status->setText(tr("Please wait..."));

OleksandrBlack/safecoin-qt-wallet

src/sendtab.cpp
@@ -811,7 +811,11 @@ void MainWindow::sendButton() {
         auto d = new QDialog(this);
         auto connD = new Ui_ConnectionDialog();
         connD->setupUi(d);
-        QPixmap logo(":/img/res/logobig.gif");
+        QMovie *movie = new QMovie(":/img/res/safewallet-animated.gif");
+        connD->topIcon->setMovie(movie);
+        movie->start();
+
+
         //connD->topIcon->setBasePixmap(logo.scaled(256, 256, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 
 