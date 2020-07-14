hushmirror/SilentDragon

Dark mode has a dark animation

src/connection.cpp
@@ -19,11 +19,19 @@ ConnectionLoader::ConnectionLoader(MainWindow* main, RPC* rpc) {
     d->setWindowFlags(d->windowFlags() & ~(Qt::WindowCloseButtonHint | Qt::WindowContextHelpButtonHint));
     connD = new Ui_ConnectionDialog();
     connD->setupUi(d);
-    QMovie *movie = new QMovie(":/img/res/silentdragon-animated.gif");
-    //QPixmap logo(":/img/res/silentdragon-animated.gif");
-    connD->topIcon->setMovie(movie);
-    movie->start();
-    //main->logger->write("set topIcon");
+    QMovie *movie1 = new QMovie(":/img/res/silentdragon-animated.gif");;
+    QMovie *movie2 = new QMovie(":/img/res/silentdragon-animated-dark.gif");;
+    auto theme = Settings::getInstance()->get_theme_name();
+    if (theme == "dark") {
+        movie2->setScaledSize(QSize(512,512));
+        connD->topIcon->setMovie(movie2);
+        movie2->start();
+    } else {
+        movie1->setScaledSize(QSize(512,512));
+        connD->topIcon->setMovie(movie1);
+        movie1->start();
+    }
+    main->logger->write("set animation");
 }
 
 ConnectionLoader::~ConnectionLoader() {

src/sendtab.cpp
@@ -707,14 +707,23 @@ void MainWindow::sendButton() {
     // Show a dialog to confirm the Tx
     if (confirmTx(tx)) {
 
-          // Create a new Dialog to show that we are computing/sending the Tx
+        // Create a new Dialog to show that we are computing/sending the Tx
         auto d = new QDialog(this);
         auto connD = new Ui_ConnectionDialog();
         connD->setupUi(d);
-        QMovie *movie = new QMovie(":/img/res/silentdragon-animated.gif");
-        connD->topIcon->setMovie(movie);
-        movie->start();
 
+        QMovie *movie1 = new QMovie(":/img/res/silentdragon-animated.gif");;
+        QMovie *movie2 = new QMovie(":/img/res/silentdragon-animated-dark.gif");;
+        auto theme = Settings::getInstance()->get_theme_name();
+        if (theme == "dark") {
+            movie2->setScaledSize(QSize(512,512));
+            connD->topIcon->setMovie(movie2);
+            movie2->start();
+        } else {
+            movie1->setScaledSize(QSize(512,512));
+            connD->topIcon->setMovie(movie1);
+            movie1->start();
+        }
 
         //connD->topIcon->setBasePixmap(logo.scaled(256, 256, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 

OleksandrBlack/safecoin-qt-wallet

src/connection.cpp
@@ -20,11 +20,19 @@ ConnectionLoader::ConnectionLoader(MainWindow* main, RPC* rpc) {
     d = new QDialog(main);
     connD = new Ui_ConnectionDialog();
     connD->setupUi(d);
-    QMovie *movie = new QMovie(":/img/res/safewallet-animated.gif");
-    //QPixmap logo(":/img/res/safewallet-animated.gif");
-    connD->topIcon->setMovie(movie);
-    movie->start();
-    //main->logger->write("set topIcon");
+    QMovie *movie1 = new QMovie(":/img/res/safewallet-animated.gif");;
+    QMovie *movie2 = new QMovie(":/img/res/safewallet-animated-dark.gif");;
+    auto theme = Settings::getInstance()->get_theme_name();
+    if (theme == "dark") {
+        movie2->setScaledSize(QSize(512,512));
+        connD->topIcon->setMovie(movie2);
+        movie2->start();
+    } else {
+        movie1->setScaledSize(QSize(512,512));
+        connD->topIcon->setMovie(movie1);
+        movie1->start();
+    }
+    main->logger->write("set animation");
 }
 
 ConnectionLoader::~ConnectionLoader() {    

src/sendtab.cpp
@@ -804,17 +804,27 @@ void MainWindow::sendButton() {
             recurringPaymentHash = sendTxRecurringInfo->getHash();
         }
 
+
         // Then delete the additional fields from the sendTo tab
         clearSendForm();
 	
-          // Create a new Dialog to show that we are computing/sending the Tx
+
+        // Create a new Dialog to show that we are computing/sending the Tx
         auto d = new QDialog(this);
         auto connD = new Ui_ConnectionDialog();
         connD->setupUi(d);
-        QMovie *movie = new QMovie(":/img/res/safewallet-animated.gif");
-        connD->topIcon->setMovie(movie);
-        movie->start();
-
+        QMovie *movie1 = new QMovie(":/img/res/safewallet-animated.gif");;
+        QMovie *movie2 = new QMovie(":/img/res/safewallet-animated-dark.gif");;
+        auto theme = Settings::getInstance()->get_theme_name();
+        if (theme == "dark") {
+            movie2->setScaledSize(QSize(512,512));
+            connD->topIcon->setMovie(movie2);
+            movie2->start();
+        } else {
+            movie1->setScaledSize(QSize(512,512));
+            connD->topIcon->setMovie(movie1);
+            movie1->start();
+        }
 
         //connD->topIcon->setBasePixmap(logo.scaled(256, 256, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 