59ff8e4dd728d91a92f70d46def75d802ca5ca55
Sol <safe@safecoin.org>
2020-07-29 03:38:46 -0600

    theme update, add Midnight from Charles Sharpe

--- src/connection.cpp
+++ src/connection.cpp
@@ -22,7 +22,7 @@ ConnectionLoader::ConnectionLoader(MainWindow* main, RPC* rpc) {
     QMovie *movie1 = new QMovie(":/img/res/safecoindlogo.gif");;
     QMovie *movie2 = new QMovie(":/img/res/safecoindlogo.gif");;
     auto theme = Settings::getInstance()->get_theme_name();
-    if (theme == "dark") {
+    if (theme == "dark" || "midnight") {
         movie2->setScaledSize(QSize(256,256));
         connD->topIcon->setMovie(movie2);
         movie2->start();
