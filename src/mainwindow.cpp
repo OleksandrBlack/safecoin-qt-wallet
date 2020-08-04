760b798557d6d876cf4418875e2417fe9ca969c5
Lucky-Mining <aleksandr.chornuy@gmail.com>
2020-08-04 16:57:06 +0300

    fix midnight, fix dropdowns

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -396,7 +396,7 @@ void MainWindow::setupSettingsModal() {
         QDir zcashdir(rpc->getConnection()->config->zcashDir);
         QFile WalletSize(zcashdir.filePath("wallet.dat"));
         if (WalletSize.open(QIODevice::ReadOnly)){
-        size = WalletSize.size() / 1000000;  //when file does open.
+        size = WalletSize.size() / 1000;  //when file does open.
         //QString size1 = QString::number(size) ;
         settings.WalletSize->setText(QString::number(size));
         WalletSize.close();
@@ -1259,11 +1259,11 @@ void MainWindow::SafeNodesTab() {
     QMovie *movie2 = new QMovie(":/img/res/safenodelogo.gif");;
     auto theme = Settings::getInstance()->get_theme_name();
     if (theme == "dark") {
-        movie2->setScaledSize(QSize(256,256));
+        movie2->setScaledSize(QSize(128,128));
         ui->safenodelogo->setMovie(movie2);
         movie2->start();
     } else {
-        movie1->setScaledSize(QSize(256,256));
+        movie1->setScaledSize(QSize(128,128));
         ui->safenodelogo->setMovie(movie1);
         movie1->start();
     }
